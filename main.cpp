60b3c2b97905dbcf0028bfa52ffb98d3610031fb
dacharyc <dc@dacharycarey.com>
2024-02-16 20:00:42 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/screen/screen.hpp"
+#include "ftxui/screen/string.hpp"
+
+int main(void) {
+  using namespace ftxui;
+
+  auto document =  //
+      vbox({
+          text(L" CPP Tomagotchi ") | bold | hcenter,
+          text(L" Adopt and care for a friend! ") | bold | hcenter
+      }) | border;
+
+  // Limit the size of the document to 80 char.
+  document = document | size(WIDTH, LESS_THAN, 80);
+
+  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+  Render(screen, document);
+
+  std::cout << screen.ToString() << '\0' << std::endl;
+
+  return EXIT_SUCCESS;
+}
